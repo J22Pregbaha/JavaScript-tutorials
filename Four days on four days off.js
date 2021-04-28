3eae5645a60304d57823f529a87e8e0d8649ac0f
David <42592467+protege47007@users.noreply.github.com>
2021-04-28 17:48:54 +0100

    Update Four days on four days off.js
    just a little spicing up to get current automatically and also account for weekends since we don't work on weekends ;)

--- Four days on four days off.js
+++ Four days on four days off.js
@@ -1,4 +1,4 @@
-// I started a new job and the shift pattern is 4 days on and 4 days off so I wanted to c=be able to check which days I'm on and which days I'm off in the year 2020
+// I started a new job and the shift pattern is 4 days on and 4 days off so I wanted to c=be able to check which days I'm on and which days I'm off in the year 2020 and 2021
 
 let startDate = new Date("2020-10-28"); // The date I started the shift
 
@@ -9,26 +9,35 @@ const daysOff = []; // The dates I will be off
 
 addDaysWorking(daysOnAdnOff);
 
-function addDaysWorking(daysOnAdnOff) {
-	for (var i = 0; i < daysOnAdnOff; i++) {
-		daysWorking.push(startDate.getDate() + "-" + (startDate.getMonth() + 1) + "-" + startDate.getFullYear());
-		startDate.setDate(startDate.getDate() + 1);
-	}
+function addDaysWorking(daysOnAndOff) {
+    if (startDate.getDay() === 6 || startDate.getDay() === 0){ //this accounts for the weekends
+        startDate.setDate(startDate.getDate() +1);
+    }
+    else{
+        for (var i = 0; i < daysOnAndOff; i++) {
+            daysWorking.push(startDate.getDate() + "-" + (startDate.getMonth() + 1) + "-" + startDate.getFullYear());
+            startDate.setDate(startDate.getDate() + 1);
+        }
+    }
 
-	startDate.getFullYear() === 2020 ? addDaysOff(daysOnAdnOff) : null;
+    startDate.getFullYear() === 2021 ? addDaysOff(daysOnAndOff) : null;
 }
 
-function addDaysOff(daysOnAdnOff) {
-	for (var i = 0; i < daysOnAdnOff; i++) {
-		daysOff.push(startDate.getDate() + "-" + (startDate.getMonth() + 1) + "-" + startDate.getFullYear());
-		startDate.setDate(startDate.getDate() + 1);
-	}
+function addDaysOff(daysOnAndOff) {
+    for (var i = 0; i < daysOnAndOff; i++) {
+        daysOff.push(startDate.getDate() + "-" + (startDate.getMonth() + 1) + "-" + startDate.getFullYear());
+        startDate.setDate(startDate.getDate() + 1);
+    }
 
-	startDate.getFullYear() === 2020 ? addDaysWorking(daysOnAdnOff) : null;
+    startDate.getFullYear() === 2021 ? addDaysWorking(daysOnAndOff) : null;
 }
+
+
 
 function checkDate(date) {
-	daysWorking.includes(date) ? console.log("Working") : console.log("Day off");
+    daysWorking.includes(date) ? console.log("Working") : console.log("Day off");
 }
 
-checkDate("25-12-2020"); // Date format is D-MM-YYYY
+let currentDate = new Date(); // automatically gets the current date to know if i should sleep or get out of bed
+let x = currentDate.getDate() +"-"+ (currentDate.getMonth() +1) +"-"+ currentDate.getFullYear();
+checkDate(x); // Date format is D-MM-YYYY
